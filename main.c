f0d8dc1266126016e0f27759b7d712522a66c4d3
timikid <timikit@naver.com>
2020-09-17 17:33:26 +0900

    03-4

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	char c;
+	int a,b;
 	
-	printf("input an alphbet : ");
-	scanf("%c", &c);
+	printf("input two integers : ");
+	scanf("%d %d", &a, &b);
 	
-	printf("input alphabet is %c (%d), and the next one is %c (%d)\n", c, c, c+1, c+1);
+	printf("result is %f\n", (float)a/b);
 	
 	return 0;
 }
